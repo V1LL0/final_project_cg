8b1b557281b6d9932a470a4b48cca12fea9bda76
massimo <v.cestarelli@gmail.com>
2015-01-31 17:31:26 +0100

    fix zeroclipboard

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,17 +1,18 @@
 var clientTarget = new ZeroClipboard( $("#target-to-copy"), {
-	console.log("entro qui");
     moviePath: "./ZeroClipboard.swf",
     debug: false
 } );
+
+console.log("entro qui");
 
 clientTarget.on( "load", function(clientTarget)
 {
     
     //$('#flash-loaded').fadeIn();
-
+	console.log("entro qui 2");
     clientTarget.on( "complete", function(clientTarget, args) {
         clientTarget.setText( args.text );
-        console.log("sono qui: "+args.text);
+        console.log("sono qui: "+ args.text);
         //$('#target-to-copy-text').fadeIn();
     } );
 } );
