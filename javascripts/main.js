132838227c16361d9074c1e078c01482889d83d9
massimo <v.cestarelli@gmail.com>
2015-01-31 17:05:55 +0100

    aggiunto zeroclipboard

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,26 @@
-console.log('This would be the main JS file.');
+// main.js
+var client = new ZeroClipboard( document.getElementById("copy-button"), {
+
+moviePath: "/flash/ZeroClipboard.swf"
+
+} );
+
+ 
+
+client.on( "load", function(client) {
+
+// alert( "movie is loaded" );
+
+ 
+
+client.on( "complete", function(client, args) {
+
+// `this` is the element that was clicked
+
+this.style.display = "none";
+
+alert("Copied text to clipboard: " + args.text );
+
+} );
+
+} );
