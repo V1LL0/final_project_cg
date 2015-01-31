c4b02f1396150648f8b8a8a97570b26ba5d8a0d2
massimo <v.cestarelli@gmail.com>
2015-01-31 15:29:00 +0100

    prova fix plasm chrome

---
+++ javascripts/copyToClipboard.js
@@ -0,0 +1,39 @@
+function copyLC7() {
+    var str = document.getElementById("LC7_text").value;
+    var LC7_text = $('#LC7_text').val(str).select();
+    document.execCommand('copy');
+};
+
+document.getElementById("copyLC7").onclick = this.copyLC7;
+
+function copyLC11() {
+    var str = document.getElementById("LC11_text").value;
+    var LC11_text = $('#LC11_text').val(str).select();
+    document.execCommand('copy');
+};
+
+document.getElementById("copyLC11").onclick = this.copyLC11;
+
+function copyLC17() {
+    var str = document.getElementById("LC17_text").value;
+    var LC17_text = $('#LC17_text').val(str).select();
+    document.execCommand('copy');
+};
+
+document.getElementById("copyLC17").onclick = this.copyLC17;
+
+function copyVoitureMinimum() {
+    var str = document.getElementById("VoitureMinimum_text").value;
+    var VoitureMinimum_text = $('#VoitureMinimum_text').val(str).select();
+    document.execCommand('copy');
+};
+
+document.getElementById("copyVoitureMinimum").onclick = this.copyVoitureMinimum;
+
+function copyAllInAHouse() {
+    var str = document.getElementById("AllInAHouse_text").value;
+    var AllInAHouse_text = $('#AllInAHouse_text').val(str).select();
+    document.execCommand('copy');
+};
+
+document.getElementById("copyAllInAHouse").onclick = this.copyAllInAHouse;
