ba8e05d215207ed43308ba914db5857092f14569
massimo <v.cestarelli@gmail.com>
2015-01-31 17:24:34 +0100

    fix zeroclipboard

--- javascripts/main.js
+++ javascripts/main.js
@@ -5,10 +5,12 @@ var clientTarget = new ZeroClipboard( $("#target-to-copy"), {
 
 clientTarget.on( "load", function(clientTarget)
 {
+    
     $('#flash-loaded').fadeIn();
 
     clientTarget.on( "complete", function(clientTarget, args) {
         clientTarget.setText( args.text );
+        console.log("sono qui: "+args.text);
         $('#target-to-copy-text').fadeIn();
     } );
 } );
