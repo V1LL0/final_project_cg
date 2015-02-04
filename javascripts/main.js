e18b8f4c8123731dea60f66d298ff59b5d7977bc
massimo <v.cestarelli@gmail.com>
2015-02-04 14:47:07 +0100

    added others

--- javascripts/main.js
+++ javascripts/main.js
@@ -43,16 +43,16 @@ $(document).ready(function() {
   var clip = new ZeroClipboard($("#target-to-copy"));
 
   clip.on("ready", function() {
-    debugstr("Flash movie loaded and ready.");
+    console.log("Flash movie loaded and ready.");
 
     this.on("aftercopy", function(event) {
-      debugstr("Copied text to clipboard: " + event.data["text/plain"]);
+      console.log("Copied text to clipboard: " + event.data["text/plain"]);
     });
   });
 
   clip.on("error", function(event) {
     $(".demo-area").hide();
-    debugstr('error[name="' + event.name + '"]: ' + event.message);
+    console.log('error[name="' + event.name + '"]: ' + event.message);
     ZeroClipboard.destroy();
   });
 
