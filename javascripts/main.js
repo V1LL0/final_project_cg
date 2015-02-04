b32f00fb6e9e4f4e4f88cb6600ae705d2f4f397e
massimo <v.cestarelli@gmail.com>
2015-02-04 14:44:54 +0100

    added others

--- javascripts/main.js
+++ javascripts/main.js
@@ -21,7 +21,7 @@ clientTarget.onload = function(clientTarget)
 */
 
 
-
+/*
 
 
 var clipone = new ZeroClipboard( document.getElementById("target-to-copy"), {
@@ -34,3 +34,38 @@ clipone.on( 'complete', function(client, args) {
 	alert('Clicked ' + this.id);
 	
 } );
+
+*/
+
+
+$(document).ready(function() {
+
+  var clip = new ZeroClipboard($("#target-to-copy"));
+
+  clip.on("ready", function() {
+    debugstr("Flash movie loaded and ready.");
+
+    this.on("aftercopy", function(event) {
+      debugstr("Copied text to clipboard: " + event.data["text/plain"]);
+    });
+  });
+
+  clip.on("error", function(event) {
+    $(".demo-area").hide();
+    debugstr('error[name="' + event.name + '"]: ' + event.message);
+    ZeroClipboard.destroy();
+  });
+
+
+  // jquery stuff (optional)
+  /*
+  function debugstr(text) {
+    $("#d_debug").append($("<p>").text(text));
+  }
+
+  $("#clear-test").on("click", function () {
+    $("#fe_text").val("Copy me!");
+    $("#testarea").val("");
+  });
+*/
+});
