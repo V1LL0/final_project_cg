6e2302e734fc61d78632bf87d34d1ff26568c1e4
massimo <v.cestarelli@gmail.com>
2015-02-04 17:05:38 +0100

    added others

--- javascripts/main.js
+++ javascripts/main.js
@@ -40,31 +40,84 @@ clipone.on( 'complete', function(client, args) {
 
 $(document).ready(function() {
 
-  var clip = new ZeroClipboard($("#target-to-copy"));
+  var clip1 = new ZeroClipboard($("#target-to-copy-lc17"));
 
-  clip.on("ready", function() {
+  clip1.on("ready", function() {
     console.log("Flash movie loaded and ready.");
 
     this.on("aftercopy", function(event) {
     });
   });
 
-  clip.on("error", function(event) {
+  clip1.on("error", function(event) {
     $(".demo-area").hide();
     console.log('error[name="' + event.name + '"]: ' + event.message);
     ZeroClipboard.destroy();
   });
 
 
-  // jquery stuff (optional)
-  /*
-  function debugstr(text) {
-    $("#d_debug").append($("<p>").text(text));
-  }
+  var clip2 = new ZeroClipboard($("#target-to-copy-lc11"));
 
-  $("#clear-test").on("click", function () {
-    $("#fe_text").val("Copy me!");
-    $("#testarea").val("");
+  clip2.on("ready", function() {
+    console.log("Flash movie loaded and ready.");
+
+    this.on("aftercopy", function(event) {
+    });
   });
-*/
+
+  clip2.on("error", function(event) {
+    $(".demo-area").hide();
+    console.log('error[name="' + event.name + '"]: ' + event.message);
+    ZeroClipboard.destroy();
+  });
+
+
+
+  var clip3 = new ZeroClipboard($("#target-to-copy-lc7"));
+
+  clip3.on("ready", function() {
+    console.log("Flash movie loaded and ready.");
+
+    this.on("aftercopy", function(event) {
+    });
+  });
+
+  clip3.on("error", function(event) {
+    $(".demo-area").hide();
+    console.log('error[name="' + event.name + '"]: ' + event.message);
+    ZeroClipboard.destroy();
+  });
+
+
+    var clip4 = new ZeroClipboard($("#target-to-copy-voiture-minimum"));
+
+  clip4.on("ready", function() {
+    console.log("Flash movie loaded and ready.");
+
+    this.on("aftercopy", function(event) {
+    });
+  });
+
+  clip4.on("error", function(event) {
+    $(".demo-area").hide();
+    console.log('error[name="' + event.name + '"]: ' + event.message);
+    ZeroClipboard.destroy();
+  });
+
+    var clip5 = new ZeroClipboard($("#target-to-copy-all-in-a-house"));
+
+  clip5.on("ready", function() {
+    console.log("Flash movie loaded and ready.");
+
+    this.on("aftercopy", function(event) {
+    });
+  });
+
+  clip5.on("error", function(event) {
+    $(".demo-area").hide();
+    console.log('error[name="' + event.name + '"]: ' + event.message);
+    ZeroClipboard.destroy();
+  });
+
+
 });
